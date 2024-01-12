a98e6e85aadaa564d654fd584485821718829b60
Radamés <radames.idiomas@gmail.com>
2024-01-11 22:00:53 -0300

    cria a função que retorna a classificação do jogador

--- main.js
+++ main.js
@@ -24,3 +24,30 @@ function getPlayerStats(wins, losses) {
   }
   return result;
 }
+
+// função que retorna a classificação do jogador
+function getPlayerRank(){
+
+  let totalWins = getPlayerStats();
+  let ranking = '';
+
+  if (totalWins > 0 && totalWins <= 10) {
+    ranking = "Ferro";
+  } else if (totalWins >= 11 && totalWins <= 20) {
+    ranking = "Bronze";
+  } else if (totalWins >= 21 && totalWins <= 50) {
+    ranking = "Prata";
+  } else if (totalWins >= 51 && totalWins <= 80) {
+    ranking = "Ouro";
+  } else if (totalWins >= 81 && totalWins <= 90) {
+    ranking = "Diamante";
+  } else if (totalWins >= 91 && totalWins <= 100) {
+    ranking = "Lendário";
+  } else if (totalWins >= 101) {
+    ranking = "Imortal";
+  } else {
+    ranking = "Inativo";
+  }
+
+  return `O Herói tem de saldo de vitórias: ${totalWins} e está no nível de ${ranking}.`;
+}
