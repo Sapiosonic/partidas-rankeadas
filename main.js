2b31df077db1560a452ca6b44e80acc3b7f0600d
Radamés <radames.idiomas@gmail.com>
2024-01-11 21:59:47 -0300

    criando função que retorna o número total de vitórias

--- main.js
+++ main.js
@@ -2,3 +2,25 @@
 function getRandomNumber() {
   return Math.floor(Math.random() * 200) + 1;
 }
+
+// função que gera número de vitórias
+function getPlayerStats(wins, losses) {
+  let result = 0;
+  let plays = 10;
+  wins = getRandomNumber();
+  losses = getRandomNumber(); 
+  
+  for(let i = 0; i <= plays; i++){
+    if(i % 2 == 0){
+      wins++;
+    }else{
+      losses++;
+    }
+  }
+  result = wins - losses;
+
+  if(result < 0){
+    result *= -1;
+  }
+  return result;
+}
